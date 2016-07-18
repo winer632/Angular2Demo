5df77dd1cb063d3d5d2c5b1d9d3bc4ae0448a253
miaoyaoyao <miaoyaoyao01@163.com>
2016-07-18 19:45:32 +0800

    tour-of-heroes-step2

--- angular2-tour-of-heroes/README.md
+++ angular2-tour-of-heroes/README.md
@@ -3,6 +3,46 @@
 ## ng2与ng1区别
 
 * ng-model 变成[(ngModal)]
+* *ngFor="let hero of heroes"
+* *ngIf="selectedHero"
 
-> 以前在ng1中如{{ name }}在ng中更多的是如下格式{{name}},中间没有空格了，你只用记住跟{}或()相关的都没有空格了
+* (click)="fun()" // 方法
+* [class.selected]="hero === selectedHero" // 属性绑定
+
+``` javascript
+// @Component中的styles示例
+
+styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+`]
+
+// @Component中的template示例
+template: `
+  <h1>{{title}}</h1>
+  <h2>My Heroes</h2>
+  <ul class="heroes">
+    <li *ngFor="let hero of heroes">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>
+`
+```
+
+
+> 以前在ng1中如{{ name }}在ng中更多的是如下格式{{name}},中间没有空格了，你只用记住跟{}或()相关的都没有空格了    
+
+> template支持es6中的``,写template确实比较舒服
+
+
+
+## 不同branch
+
+``` bash
+git branch tour-of-heroes-step1
+git push origin tour-of-heroes-step1
+git check out tour-of-heroes-step1
+```
 
--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -4,26 +4,106 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 
 
 @Component({
   selector: 'my-app',
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `],
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
 
-    <div>
-      <label>name: </label>
-      <input type="text" placeholder="name" [(ngModel)]="hero.name" />
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>
+
   `
 })
 export class AppComponent {
-  // properties
-  // a title property for the application name
-  // a hero property for a hreo name "Windstorm"
+
+  public heroes = HEROES;
+
   title = 'Tour of Heroes';
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) {
+    console.log('aaa');
+
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
 
   hero: Hero = {
     id: 1,
@@ -31,4 +111,6 @@ export class AppComponent {
   }
 
 
+
 }
+
